68be36b3a3204286c521d370d0c94cf8d2f97cb0
AmruthaThotakura <venkateswararaot27@gmail.com>
2025-05-23 18:17:16 +0530

    pushing

---
+++ script.js
@@ -0,0 +1,16 @@
+let generateButton = document.getElementById("generate-button")
+let passwordInput = document.getElementById("password")
+
+function generatePassword() {
+
+    let password = ""
+    let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+[]{}|;:,.<>?/~`"
+
+    for (let i = 0; i < 8; i++) {
+        password += possible.charAt(Math.floor(Math.random() * possible.length))
+    }
+
+    passwordInput.value = password
+    }
+
+    generateButton.addEventListener("click", generatePassword)
